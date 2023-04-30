1ec8346249373dd56449713fe247738cf60671f1
cuongdm3 <cuongpigerr@gmail.com>
2023-05-01 01:05:27 +0700

    This is Manh Cuong's commit 🌻.

--- cmd/docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-function install_extended_packages {
-  required_file=$(pwd)/extended_requirements.txt
-  SETUPTOOLS_USE_DISTUTILS=stdlib python3 -m pip --no-cache-dir install -r $required_file
-}
