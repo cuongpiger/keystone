1ec0d8c3cde788ea3189da28c8f9f608c5703b9d
cuongdm3 <cuongpigerr@gmail.com>
2023-05-10 14:51:35 +0700

    This is Manh Cuong's commit 🌻.

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from keystone.server.wsgi import initialize_public_application
 
 app = initialize_public_application()
 # print(app.url_map)
-app.run(debug=True)
+app.run(debug=True, host='0.0.0.0', port=5000)
